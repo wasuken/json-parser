fb23ab38f66648b0745317a8c2d65da70cc1322e
wasuken <wevorence@gmail.com>
2019-08-16 15:15:02 +0900

    added parse-float, and removed clojure-html. and added true/false test.

--- t/json-parser-test.lisp
+++ t/json-parser-test.lisp
@@ -3,7 +3,7 @@
   (:use :cl :prove :json-parser))
 (in-package #:json-parser-test)
 
-(subtest "lexer"
+(subtest "based test"
   (is (json-parser:parse "{\"test\": \"test\"}")
 	  '(:|test| "test"))
   (is (json-parser:parse "{\"test\": \"test\", \"test2\": \"test2\"}")
@@ -16,4 +16,18 @@
 	  '(:|test| "te:st" :|test2| "t:est2" :|test3| (:|test4| "test:4")))
   (is (json-parser:parse "{\"test\": 1, \"test2\": 2}")
 	  '(:|test| 1 :|test2| 2))
+  (is (json-parser:parse "{\"test\": true, \"test2\": false}")
+	  '(:|test| t :|test2| nil))
   )
+
+;; (subtest "error mechanism"
+;;   (is-error (json-parser:parse "{\"test\":")  'simple-error)
+;;   (is-error (json-parser:parse "")  'simple-error)
+;;   (is-error (json-parser:parse "}}}}}}}")  'simple-error)
+;;   (is-error (json-parser:parse ",,,,,,,{,,,,}{:,a}") 'simple-error))
+
+;; (subtest "mean test"
+;;   (is (json-parser:parse "{1:1}")
+;; 	  '(:|1| 1))
+;;   (is (json-parser:parse "{false:false}")
+;; 	  '(:|false| false)))
